80fdc454ea1555340f3638c82ad102bd601db10b
dgamboav <diegoegamboa@gmail.com>
2023-07-04 18:07:35 -0400

    First commit with java sources

---
+++ UCABOpenRewrite/src/main/java/openrewrite/SimpleSystemOutExample.java
@@ -0,0 +1,26 @@
+package openrewrite;
+
+import java.util.Scanner;
+
+public class SimpleSystemOutExample {
+
+	public static void main(String[] args) {		
+		
+		Scanner console = new Scanner(System.in);
+		System.out.print("Introduce tu nombre: ");
+        String name = console.nextLine();
+		
+        SimpleSystemOutExample ssoe = new SimpleSystemOutExample();
+        ssoe.print(name);
+
+        console.close();        
+	}
+	
+	public void print(String name) {
+        /*‮ } ⁦ dirrection  ⁩ ⁦ begin welcome message */
+		System.out.println("Welcome "+ name);
+		System.out.println("Entering the system.");
+		  /* welcome message ending ‮ { ⁦ */
+	}
+
+}
